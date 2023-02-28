e65d3ca773476228187e07bfc527c87fdcb9fd6e
Revealing later! <83834491+FailedCoder23@users.noreply.github.com>
2023-02-28 17:29:50 -0500

    Initialize dependabot/npm_and_yarn/willsCheatMenu/path-parse-1.0.7

---
+++ hacks/archive/chatPhraseMaker.js
@@ -0,0 +1,6 @@
+//> Chat Phrase Maker
+//>> A custom chat hack. Only client side.
+
+// NOTE: This hack is archived and no longer works.
+
+_.instance.prodigy.gameContainer.get("ChatManager").Y.push("INSERT TEXT HERE");
---
+++ hacks/misc/setBountyScore.js
@@ -0,0 +1,3 @@
+//> Bounty score hack
+//>> Sets your bounty score to 100.
+_.player.data.bountyScore = 100;
